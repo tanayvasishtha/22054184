20a73c198fb2a25b63c7bd29e60d384e38a7917c
Tanay Vasishtha <tanayvasishtha@gmail.com>
2025-04-02 19:38:52 +0530

    Update App.js to set up routing between components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import TopUsers from './components/TopUsers';
+import TrendingPosts from './components/TrendingPosts';
+import Feed from './components/Feed';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<Feed />} />
+            <Route path="/top-users" element={<TopUsers />} />
+            <Route path="/trending-posts" element={<TrendingPosts />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
